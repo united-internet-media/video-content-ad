936f8a4d12409d08ac3e96519e724569dc15020a
Tobias Baron <tobias.baron@united-internet-media.de>
2026-03-02 15:26:09 +0100

    chore: add use strict directive to vca.js

--- vca.js
+++ vca.js
@@ -1,4 +1,5 @@
 (function() {
+    'use strict';
     var playerIframe, iframeContainer, iframeContainerChild, playbackTimeout,
     intersectionObserver, eventSource, eventOrigin, closeButton, currentVideoTitle,
 	articleObserver, article, belowArticle, inSticky,
